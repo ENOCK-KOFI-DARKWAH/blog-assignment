2c864fdd5a7b9b6574761340c61b7634dc20e09a
Enock-Kofi_Darkwah <darkwahenock@gmail.com>
2021-10-29 17:26:25 +0000

    done

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const connectDb = () => {
   return mongoose.connect(DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true }, err => {
     if (err) {
-      console.log("Connection to Database failed.");
+      console.log("Database Connection failed.");
     }
     else{
       console.log("Database connection successful.");
